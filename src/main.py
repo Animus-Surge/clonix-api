a1ed58e4e9f486a7b8181c4d4cc9f88a9ccfade3
Surge <esfloyd9341@gmail.com>
2026-07-31 16:17:04 -0400

    Start database schema objects

--- src/main.py
+++ src/main.py
@@ -1,26 +1,33 @@
 from loguru import logger
 
 from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse
 
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+
+
 # Authentication endpoints
-@app.get("/api/v1/auth/login/")
-def login():
+@app.get("/api/v1/auth/login")
+async def login(req: Request):
     pass
 
-@app.post("/api/v1/auth/callback")
-def saml_Callback():
+@app.post("/api/v1/auth/saml/acs")
+async def saml_callback():
+    pass
+
+@app.get("/api/v1/auth/saml/metadata")
+async def saml_metadata():
     pass
 
 # Primary Endpoints
 @app.get("/api/v1/")
-def index():
+async def index():
     logger.info("GET: / Root index get")
     return "Hello!"
 
 @app.get("/api/v1/devices/")
-def devices():
+async def devices():
     pass
---
+++ src/schema.py
@@ -0,0 +1,12 @@
+"""
+Database schema definition
+"""
+
+from sqlalchemy import MetaData, Table, Column, Integer, String
+
+# Metadata object
+metadata_obj = MetaData()
+
+# Table decl
+
+
