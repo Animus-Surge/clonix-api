4f19831640407a436889d5ca49728ed3edb0f3ff
Surge <esfloyd9341@gmail.com>
2026-07-31 12:32:59 -0400

    SAML: Add endpoints.

--- src/main.py
+++ src/main.py
@@ -1,9 +1,21 @@
 from loguru import logger
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# Authentication endpoints
+@app.get("/api/v1/auth/login/")
+def login():
+    pass
+
+@app.post("/api/v1/auth/callback")
+def saml_Callback():
+    pass
+
+# Primary Endpoints
 @app.get("/api/v1/")
 def index():
     logger.info("GET: / Root index get")
